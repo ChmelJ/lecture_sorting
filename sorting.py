8229585c1fb7cce0e76a2553f2ed413e55c09328
ChmelJ <chmelikj@vutbr.cz>
2023-04-16 14:59:47 +0200

    Funkcni selection sort

--- sorting.py
+++ sorting.py
@@ -15,13 +15,27 @@ def read_data(file_name):
         reader = csv.reader(csv_file)
         keys = next(reader)
         data = [dict(zip(keys, i)) for i in reader]
-    data_dictionary = {i: [c[i] for c in data] for i in keys}
+    data_dictionary = {i: [int(c[i]) for c in data] for i in keys}
     return data_dictionary
 
 
+def selection_sorting(list_of_numbers):
+    for i in range(len(list_of_numbers)):
+        smallest = float('inf')
+        ind_min = i
+        for ind, number in enumerate(list_of_numbers[i:]):
+            if number < smallest:
+                smallest = number
+                ind_min = ind+i
+        list_of_numbers[i], list_of_numbers[ind_min] = list_of_numbers[ind_min], list_of_numbers[i]
+    return list_of_numbers
+
+
 def main():
-    my_data =read_data('numbers.csv')
-    print(my_data)
+    my_data = read_data('numbers.csv')
+    selection_sorted_numbers = selection_sorting(my_data['series_3'])
+    print(my_data['series_3'])
+    print(selection_sorted_numbers)
 
 
 if __name__ == '__main__':
